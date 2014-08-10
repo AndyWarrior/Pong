caab0f9b7ce5f1023b1bade45d1f324e659ea9e1
Andres De Leon <agldeleon@gmail.com>
2014-08-10 02:10:40 -0500

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,28 @@
+--calculate the aspect ratio of the device:
+local aspectRatio = display.pixelHeight / display.pixelWidth
+
+application = {
+   content = {
+      width = aspectRatio > 1.5 and 320 or math.ceil( 480 / aspectRatio ),
+      height = aspectRatio < 1.5 and 480 or math.ceil( 320 * aspectRatio ),
+      scale = "letterBox",
+      fps = 60,
+      graphicsCompatibility = 1,
+      xAlign = "center",
+      yAlign = "center",
+
+      imageSuffix = {
+         ["@2x"] = 1.5,
+         ["@4x"] = 3.0,
+      },
+   },
+
+    notification = {
+            iphone = {
+                types = {
+                    "badge", "sound", "alert"
+                }
+            }
+    }
+
+}
