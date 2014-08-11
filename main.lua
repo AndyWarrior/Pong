111849a2d9babd42690a3d7dfbf71690c1d87926
Andres De Leon <agldeleon@gmail.com>
2014-08-11 00:03:43 -0500

    Add side lines of the court

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+display.setStatusBar( display.HiddenStatusBar )
+
 local topY = display.screenOriginY --Numerical value for the top of the screen
 local rightX = display.contentWidth - display.screenOriginX --Numerical value for the right of the screen
 local bottomY = display.contentHeight - display.screenOriginY --Numerical value for the bottom of the screen
@@ -9,3 +11,9 @@ player1 = display.newRect(leftX, topY + screenH/2 - screenH/10, screenW/20, scre
 player2 = display.newRect(rightX - screenW/20, topY + screenH/2 - screenH/10, screenW/20, screenH/5)
 
 ball = display.newCircle(leftX + screenW/2, topY + screenH/2, screenW/40)
+
+topLine = display.newLine(leftX + screenW/20, topY, rightX - screenW/20, topY)
+topLine.width = screenH/10
+
+bottomLine = display.newLine(leftX + screenW/20, bottomY, rightX - screenW/20, bottomY)
+bottomLine.width = screenH/10
